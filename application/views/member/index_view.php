96a8cf169be0edf1c9466dec9345ae25388bd519
icharge <charge_n@hotmail.com>
2015-01-09 16:16:45 +0700

    up2

---
+++ application/views/member/index_view.php
@@ -0,0 +1,25 @@
+<!-- Begin content -->
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3 class="panel-title">ข้อมูลส่วนตัว</h3>
+		</div>
+		<div class="panel-body">
+			<dl class="dl-horizontal">
+				<dt>ชื่อผู้ใช้</dt>
+				<dd><?=$this->session->userdata('username')?></dd>
+				<dt>ชื่อในเกม</dt>
+				<dd><?=$this->session->userdata('playername')?></dd>
+				<dt>รหัสผ่าน WOL</dt>
+				<dd><?=$this->session->userdata('passpvpgn')?></dd>
+				<dt>CDKEY/Serial</dt>
+				<dd><?=$this->session->userdata('cdkey')?></dd>
+			</dl>
+		</div>
+		<div class="panel-footer text-right">
+			<a class="btn btn-primary disabled" >แก้ไข</a>
+		</div>
+	</div>
+
+</div>
+<!-- End content -->
